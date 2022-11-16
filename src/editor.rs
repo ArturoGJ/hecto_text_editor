fc10803f694bf4c6de70913fe87d67fc7a25fb91
Arturo Garrido <arturogarridoj@gmail.com>
2022-11-15 22:10:22 -0600

    Putting things into different files

---
+++ src/editor.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, self};
+
+use termion::{event::Key, raw::IntoRawMode, input::TermRead};
+
+pub struct Editor {
+}
+
+impl Editor {
+    pub fn run(&self) {
+        // Here we are using termion  to provide stdout.
+        // We are assigning the result of "into_raw_mode" to a variable named _stdout
+        // into_raw_mode modifies the terminal and returns a value which, once it is removed,
+        // will reset the terminal into canonical mode - so we are keeping this variable around
+        // by binding it to _stdout.
+        let _stdout = stdout().into_raw_mode().unwrap();
+        
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        // From wikipedia: In computing, a control character or non-printing character is 
+                        // a code point (a number) in a character set, that does not represent a written 
+                        // symbol. They are used as in-band signaling to cause effects other than the 
+                        // addition of a symbol to the text.
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    },
+                    Key::Ctrl('q') => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {}
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{e}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,12 @@
-use std::io::{self, stdout};
+#![warn(clippy::all, clippy::pedantic)]
+use editor::Editor;
 
-use termion::{raw::IntoRawMode, event::Key, input::TermRead};
-
-fn die(e: std::io::Error) {
-    panic!("{e}");
-}
+mod editor;
 /*
     Following Philipp Flenker tutorial to build a text editor in Rust, to learn more about it
     Found here: https://www.philippflenker.com/hecto/
 */
 fn main() {
-    // Here we are using termion  to provide stdout.
-    // We are assigning the result of "into_raw_mode" to a variable named _stdout
-    // into_raw_mode modifies the terminal and returns a value which, once it is removed,
-    // will reset the terminal into canonical mode - so we are keeping this variable around
-    // by binding it to _stdout.
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    // From wikipedia: In computing, a control character or non-printing character is 
-                    // a code point (a number) in a character set, that does not represent a written 
-                    // symbol. They are used as in-band signaling to cause effects other than the 
-                    // addition of a symbol to the text.
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('c') => break, // This allows us to press Ctrl + c to exit.
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
